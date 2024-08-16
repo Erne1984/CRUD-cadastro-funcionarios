00887a74945fefce4147a63921ea854fd1ea28df
Erne1984 <ernestofamorim@gmail.com>
2024-08-16 10:51:55 -0300

    feat: table funcionarios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
 import './App.css';
+import TableFuncionarios from './components/TableFuncionarios/TableFuncionarios';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+
+      <TableFuncionarios/>
+
     </>
   )
 }
--- frontend/src/components/TableFuncionarios/TableFuncionarios.tsx
+++ frontend/src/components/TableFuncionarios/TableFuncionarios.tsx
@@ -1,12 +1,62 @@
+import { useEffect, useState } from 'react';
+import './TableFuncionarios.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+interface Funcionario {
+    nome: string;
+    cpf: string;
+    funcao: string;
+    dataAdmissao: string;
+    setor: string;
+    salario: number;
+}
 
 export default function TableFuncionarios() {
+    const [funcionarios, setFuncionarios] = useState<Funcionario[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("http://localhost:8080/getFuncionarios");
+                const funcionarioList = await res.json();
+                setFuncionarios(funcionarioList);
+            } catch (err) {
+                console.log(`Erro ao buscar funcionários: ${err}`);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-        </tr>
-    )
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>CPF</th>
+                    <th>Função</th>
+                    <th>Setor</th>
+                    <th>Data de Admissão</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {funcionarios.map((funcionario) => (
+                    <tr key={funcionario.cpf}>
+                        <td>{funcionario.nome}</td>
+                        <td>{funcionario.cpf}</td>
+                        <td>{funcionario.funcao}</td>
+                        <td>{funcionario.setor}</td>
+                        <td>{new Date(funcionario.dataAdmissao).toLocaleDateString()}</td>
+                        <td className='icons'>
+                            <FontAwesomeIcon className='edit-icon' icon={faPen} />
+                            <FontAwesomeIcon className='delete-icon' icon={faTrash} />
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 }
